338cc186060dd3859b61ef29a3fca58792a6bfd9
Carlos Farias <chantal.morales@epn.edu.ec>
2019-04-16 19:36:45 -0500

    Ejercicio 2

--- ejercicio2/index.php
+++ ejercicio2/index.php
@@ -4,4 +4,21 @@
  *
  * Implementar una función que muestre un título centrado en pantalla, y la llamaremos posteriormente dos veces.
 */
-?>
+function titulo($tit){
+	
+	echo"
+		
+		<center>
+		<div align='center'>
+		<h1>$tit</h1>
+		</div>
+		</center>
+
+	";
+	
+}
+
+titulo('Carlos');
+titulo('Caicedo');
+
+
